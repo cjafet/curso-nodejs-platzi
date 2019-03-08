9a9b0178585b43f9cd67a201e20e8d2b35530b45
Carlos Jafet Neto <cjafet07@gmail.com>
2019-03-08 08:46:19 -0300

    Commit da aula 31

---
+++ 31/promise-all.js
@@ -0,0 +1,27 @@
+var request = require('request');
+const { promisify } = require('util'); // ships with node
+const get = promisify(request);
+let responses = [];
+
+async function getRequest(url) {
+    let promise = new Promise((resolve, reject) => {
+        get(url).then(response => {
+            responses.push(response.statusCode);
+            resolve();
+        })
+    });
+
+    let result = await promise;
+    return result;
+};
+
+Promise.race([
+    getRequest('http://stackoverflow.com'),
+    getRequest('http://www.google.com'),
+    getRequest('http://www.platzi.com')
+]).then(() => {
+    console.log(responses);
+})
+
+
+
